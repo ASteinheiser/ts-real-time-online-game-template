4b73c60412340b13f4e21b81c5fae6a5bbdbf31e
ASteinheiser <me@iamandrew.io>
2025-05-02 14:58:27 -0700

    replace default logos

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import './theme.css';
-import typescriptLogo from '/typescript.svg';
 import { Button } from '@repo/ui';
 import { StartGameForm } from './forms/StartGameForm';
+import './theme.css';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,11 +12,17 @@ const App = () => {
       <StartGameForm isOpen={isOpen} onOpenChange={setIsOpen} onSubmit={console.log} />
 
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
-          <img src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
+        <a
+          href="https://github.com/ASteinheiser/ts-real-time-online-game-template"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/logo.svg"
+            className="logo"
+            alt="App logo"
+            style={{ width: '120px', height: 'auto', padding: '20px', marginLeft: '20px' }}
+          />
         </a>
         <div className="card">
           <Button onClick={() => setIsOpen(true)}>Open that form!</Button>
