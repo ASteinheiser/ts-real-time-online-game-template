62982cd90d3347e09558c519884aa6e12de9c1e1
ASteinheiser <me@iamandrew.io>
2025-05-03 22:40:45 -0700

    cleanup logs for load test

--- apps/ws-server/test/load/test.ts
+++ apps/ws-server/test/load/test.ts
@@ -1,27 +1,29 @@
-import { Client, Room } from 'colyseus.js';
+import { Client } from 'colyseus.js';
 import { cli, Options } from '@colyseus/loadtest';
+import { MyRoomState } from '../../src/rooms/schema/MyRoomState';
 
 export async function main(options: Options) {
   // wait 3 seconds before starting load test
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
   const client = new Client(options.endpoint);
-  const room: Room = await client.joinOrCreate(options.roomName, {
-    // your join options here...
+  const room = await client.joinOrCreate<MyRoomState>(options.roomName);
+
+  console.log('joined room successfully!');
+
+  // add this listener otherwise colyseus will show a warning
+  room.onMessage('__playground_message_types', () => {});
+
+  room.onMessage('playerInput', (payload) => {
+    console.log('received player input: ', JSON.stringify(payload));
   });
 
-  console.log('joined successfully!');
-
-  room.onMessage('message-type', (payload) => {
-    console.log({ payload });
-  });
-
-  room.onStateChange((state) => {
-    console.log('state change:', state);
+  room.onStateChange(() => {
+    console.log(`state change at: ${Date.now()}`);
   });
 
   room.onLeave((code) => {
-    console.log('left: ', code);
+    console.log(`leaving room with code: ${code}`);
   });
 }
 
