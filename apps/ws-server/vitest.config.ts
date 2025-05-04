8124ed62d0a1877320c9d99b8fd80fe5e3cebdb4
ASteinheiser <me@iamandrew.io>
2025-05-03 17:26:47 -0700

    update vitest config

--- apps/ws-server/vitest.config.ts
+++ apps/ws-server/vitest.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     name: 'ws-server',
-    coverage: {
-      enabled: true,
-    },
+    testTimeout: 15000,
+    coverage: { enabled: false },
   },
 });
