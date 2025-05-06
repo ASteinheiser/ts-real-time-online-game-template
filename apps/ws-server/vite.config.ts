2034ff4adc605b62b52685ef39cf1219af8adb46
ASteinheiser <me@iamandrew.io>
2025-05-06 12:20:08 -0700

    fix path

--- apps/ws-server/vite.config.ts
+++ apps/ws-server/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     lib: {
-      entry: resolve(__dirname, '../src/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
       fileName: 'index',
     },
