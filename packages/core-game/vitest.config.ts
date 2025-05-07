a932f08664504280be9b7dc7030d4b45b301fe9c
ASteinheiser <me@iamandrew.io>
2025-05-06 19:53:35 -0700

    add coverage to vitest in core-game package

--- packages/core-game/vitest.config.ts
+++ packages/core-game/vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     name: 'core-game',
     watch: false,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      all: false,
+    },
   },
 });
