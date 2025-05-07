780a7ed0410b6cd6f2051155eb2dc9d96904c399
ASteinheiser <me@iamandrew.io>
2025-05-06 20:14:02 -0700

    add more tests to constants

--- packages/core-game/test/constants.test.ts
+++ packages/core-game/test/constants.test.ts
@@ -1,7 +1,35 @@
 import { describe, it, expect } from 'vitest';
-import { PLAYER_MOVE_SPEED, PLAYER_WIDTH, PLAYER_HEIGHT } from '../src/constants';
+import {
+  PLAYER_MOVE_SPEED,
+  PLAYER_WIDTH,
+  PLAYER_HEIGHT,
+  FIXED_TIME_STEP,
+  MAP_WIDTH,
+  MAP_HEIGHT,
+} from '../src/constants';
 
 describe('constants', () => {
+  describe('game', () => {
+    describe('FIXED_TIME_STEP', () => {
+      it('should be a number', () => {
+        expect(typeof FIXED_TIME_STEP).toBe('number');
+      });
+    });
+  });
+
+  describe('map', () => {
+    describe('MAP_WIDTH', () => {
+      it('should be a number', () => {
+        expect(typeof MAP_WIDTH).toBe('number');
+      });
+    });
+    describe('MAP_HEIGHT', () => {
+      it('should be a number', () => {
+        expect(typeof MAP_HEIGHT).toBe('number');
+      });
+    });
+  });
+
   describe('player', () => {
     describe('PLAYER_WIDTH', () => {
       it('should be a number', () => {
