53da0581ed9b8aa7092fcfac669c5cf3a60f10f5
ASteinheiser <me@iamandrew.io>
2025-05-06 18:42:52 -0700

    more testing calculate movement

--- packages/core-game/test/calculate-movement.test.ts
+++ packages/core-game/test/calculate-movement.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { calculateMovement } from '../src/calculate-movement';
-import { MAP_WIDTH, MAP_HEIGHT } from '../src/constants';
+import { MAP_WIDTH, MAP_HEIGHT, PLAYER_MOVE_SPEED } from '../src/constants';
 
 describe('core-game', () => {
   describe('calculateMovement', () => {
@@ -24,7 +24,12 @@ describe('core-game', () => {
             ...noMovementArgs,
           });
 
-          expect(newPosition).toEqual({ x: entity.x, y: entity.y });
+          const expectedPosition = {
+            x: entity.x,
+            y: entity.y,
+          };
+
+          expect(newPosition).toEqual(expectedPosition);
         });
       });
       describe('and the entity is out of bounds (y < 0 and x < 0)', () => {
@@ -39,7 +44,12 @@ describe('core-game', () => {
             ...noMovementArgs,
           });
 
-          expect(newPosition).toEqual({ x: 0, y: 0 });
+          const expectedPosition = {
+            x: 0,
+            y: 0,
+          };
+
+          expect(newPosition).toEqual(expectedPosition);
         });
       });
       describe('and the entity is out of bounds (y > MAP_HEIGHT and x > MAP_WIDTH)', () => {
@@ -54,7 +64,61 @@ describe('core-game', () => {
             ...noMovementArgs,
           });
 
-          expect(newPosition).toEqual({ x: MAP_WIDTH, y: MAP_HEIGHT });
+          const expectedPosition = {
+            x: MAP_WIDTH,
+            y: MAP_HEIGHT,
+          };
+
+          expect(newPosition).toEqual(expectedPosition);
+        });
+      });
+    });
+
+    describe('when the entity is moving left', () => {
+      const movementArgs = {
+        left: true,
+        right: false,
+        up: false,
+        down: false,
+      };
+
+      it('should return the entity to the left', () => {
+        const entity = {
+          x: MAP_WIDTH / 2,
+          y: MAP_HEIGHT / 2,
+        };
+
+        const newPosition = calculateMovement({
+          ...entity,
+          ...movementArgs,
+        });
+
+        const expectedPosition = {
+          x: entity.x - PLAYER_MOVE_SPEED,
+          y: entity.y,
+        };
+
+        expect(newPosition).toEqual(expectedPosition);
+      });
+
+      describe('and the entity is at the edge of the map (x = 0)', () => {
+        it('should return the entity to [0, y]', () => {
+          const entity = {
+            x: 0,
+            y: MAP_HEIGHT / 2,
+          };
+
+          const newPosition = calculateMovement({
+            ...entity,
+            ...movementArgs,
+          });
+
+          const expectedPosition = {
+            x: 0,
+            y: entity.y,
+          };
+
+          expect(newPosition).toEqual(expectedPosition);
         });
       });
     });
