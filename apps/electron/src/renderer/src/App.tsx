f49e6c1cff09b4e27b6b9e857dc6258ee75fd445
ASteinheiser <me@iamandrew.io>
2025-05-08 23:50:32 -0700

    cleanup electron styling

--- apps/electron/src/renderer/src/App.tsx
+++ apps/electron/src/renderer/src/App.tsx
@@ -1,31 +1,23 @@
 import { Versions } from './components/Versions';
 
 export const App = () => {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
+  const ipcHandle = () => {
+    window.electron.ipcRenderer.send('ping');
+  };
 
   return (
     <div>
       <h1>Powered by electron-vite</h1>
       <div>Build an Electron app with React and TypeScript</div>
-      <p>
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div>
-        <div>
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div>
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-
-      <Versions />
+      <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+        <button>Send IPC</button>
+      </a>
 
       {/* <webview src="http://localhost:4200" style={{ flexGrow: 1, width: '100%', height: '100%' }} /> */}
+
+      <div>
+        <Versions />
+      </div>
     </div>
   );
 };
