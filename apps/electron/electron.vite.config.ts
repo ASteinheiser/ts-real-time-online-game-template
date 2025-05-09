0e032da00b63881579b602dc38b8f4642b4c7750
ASteinheiser <me@iamandrew.io>
2025-05-08 18:00:13 -0700

    change build direction for electron to be dist (uniform with other repos)

--- apps/electron/electron.vite.config.ts
+++ apps/electron/electron.vite.config.ts
@@ -5,19 +5,28 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: 'dist/main',
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: 'dist/preload',
+    },
   },
   renderer: {
+    plugins: [react()],
     server: {
       port: 4208,
+    },
+    build: {
+      outDir: 'dist/renderer',
     },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
       },
     },
-    plugins: [react()],
   },
 });
