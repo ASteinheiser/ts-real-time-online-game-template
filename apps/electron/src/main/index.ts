cc1fd7309f2d9a0dd21c58f108d9d70572f8018a
ASteinheiser <me@iamandrew.io>
2025-05-08 23:48:23 -0700

    fixed preload path issue -- electron works now!

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -4,7 +4,6 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -12,7 +11,7 @@ function createWindow(): void {
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
+      preload: join(__dirname, '../preload/index.mjs'),
       sandbox: false,
     },
   });
