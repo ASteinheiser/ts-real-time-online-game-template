0ad281af656135704cc259fecb8395f3db7a5b5f
ASteinheiser <me@iamandrew.io>
2025-05-09 18:28:51 -0700

    move electron version component files around

--- apps/electron/src/renderer/src/components/Versions/Versions.test.ts
+++ apps/electron/src/renderer/src/components/Versions/Versions.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { Versions } from './Versions';
+import { Versions } from './index';
 
 describe('Versions', () => {
   it('should exist', () => {
