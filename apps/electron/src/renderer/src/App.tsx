1333c31b07b6eb68ae7c4f6ea02432aa90390605
ASteinheiser <me@iamandrew.io>
2025-05-09 18:28:28 -0700

    styled electron app

--- apps/electron/src/renderer/src/App.tsx
+++ apps/electron/src/renderer/src/App.tsx
@@ -1,4 +1,5 @@
 import { MAP_WIDTH, MAP_HEIGHT } from '@repo/core-game';
+import { Label, Button } from '@repo/ui';
 import { Versions } from './components/Versions';
 
 export const App = () => {
@@ -7,20 +8,18 @@ export const App = () => {
   };
 
   return (
-    <div>
-      <div style={{ marginBottom: '20px' }}>
-        <h1>Powered by electron-vite</h1>
-        <div>Build an Electron app with React and TypeScript</div>
-        <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-          <button>Send IPC</button>
-        </a>
+    <div className={`max-w-[${MAP_WIDTH}px] mx-auto`}>
+      <div className="flex flex-row pt-2 pb-4 justify-between items-center">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl">Powered by electron-vite</h1>
+          <Label>Build an Electron app with React and TypeScript</Label>
+        </div>
+        <Button onClick={ipcHandle}>Send IPC</Button>
       </div>
 
-      <div style={{ marginBottom: '80px' }}>
-        <iframe src="http://localhost:4200" style={{ width: MAP_WIDTH, height: MAP_HEIGHT }} />
-      </div>
+      <iframe src="http://localhost:4200" style={{ width: MAP_WIDTH, height: MAP_HEIGHT }} />
 
-      <div>
+      <div className="pt-4 pb-2">
         <Versions />
       </div>
     </div>
