de045740dfa2a3ccc4a35e7923e53f6a27c7c5e3
ASteinheiser <me@iamandrew.io>
2025-05-11 17:42:05 -0700

    organize graphql api files (schema, resolvers, etc)

--- apps/gql-server/src/server.ts
+++ apps/gql-server/src/server.ts
@@ -1,32 +1,8 @@
+import { readFileSync } from 'node:fs';
 import { ApolloServer } from '@apollo/server';
+import { resolvers } from './graphql/resolvers';
 
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+const typeDefs = readFileSync('./graphql/schema.graphql', 'utf8');
 
 export const server = new ApolloServer({
   typeDefs,
