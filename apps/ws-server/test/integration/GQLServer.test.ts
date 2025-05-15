927b1fc8b40d52ae6222c322cd97c25a206fadea
ASteinheiser <me@iamandrew.io>
2025-05-15 00:31:56 -0700

    add integration test to ws-server for gql-server

---
+++ apps/ws-server/test/integration/GQLServer.test.ts
@@ -0,0 +1,57 @@
+import assert from 'assert';
+import { server } from '../../src/graphql';
+import { BooksRepository } from '../../src/repo/Books';
+
+describe('GQLServer', () => {
+  it('should be defined', () => {
+    assert.ok(server);
+  });
+
+  it('fetches books', async () => {
+    const GET_BOOKS = `
+      query GetBooks {
+        books {
+          title
+          author
+        }
+      }
+    `;
+
+    const booksDb = new BooksRepository();
+    booksDb.getBooks = async () => [
+      {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+      },
+      {
+        id: 2,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+      },
+    ];
+
+    const result = await server.executeOperation(
+      {
+        query: GET_BOOKS,
+      },
+      {
+        contextValue: {
+          dataSources: {
+            booksDb,
+          },
+        },
+      }
+    );
+
+    const data =
+      result.body.kind === 'single'
+        ? result.body.singleResult?.data?.books
+        : result.body.initialResult.data?.books;
+
+    assert.deepEqual(data, [
+      { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+      { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    ]);
+  });
+});
