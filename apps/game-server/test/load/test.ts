7cd9feaffacc76831bb8764de00f5091f56a9e2f
ASteinheiser <me@iamandrew.io>
2025-05-18 10:23:51 -0700

    add small delay to load test for connections joining

--- apps/game-server/test/load/test.ts
+++ apps/game-server/test/load/test.ts
@@ -2,7 +2,11 @@ import { Client } from 'colyseus.js';
 import { cli, Options } from '@colyseus/loadtest';
 import { MyRoomState } from '../../src/rooms/schema/MyRoomState';
 
+const JOIN_DELAY_MS = 200;
+
 export async function main(options: Options) {
+  await new Promise((resolve) => setTimeout(resolve, JOIN_DELAY_MS));
+
   const client = new Client(options.endpoint);
   const room = await client.joinOrCreate<MyRoomState>(options.roomName);
 
