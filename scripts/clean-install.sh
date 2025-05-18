a684de5f1cbef67667e2d2e6c1726c164c159d4d
ASteinheiser <me@iamandrew.io>
2025-05-17 18:29:20 -0700

    add prisma generated files to cleanup script

--- scripts/clean-install.sh
+++ scripts/clean-install.sh
@@ -4,6 +4,7 @@ rm -rf node_modules **/*/node_modules
 rm -rf **/*/dist
 rm -rf **/*/.rollup.cache
 rm -rf **/*/coverage
+rm -rf **/*/prisma/generated
 rm -rf .turbo **/*/.turbo
 rm -f **/*/tsconfig.tsbuildinfo
 pnpm store prune
