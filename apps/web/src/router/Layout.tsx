a3348729fe8831280818a06290a4d9edfc7f7e60
ASteinheiser <me@iamandrew.io>
2025-05-25 13:37:40 -0700

    add automatic redirect to /create-profile from site layout if logged in without a profile

--- apps/web/src/router/Layout.tsx
+++ apps/web/src/router/Layout.tsx
@@ -1,7 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { TopNav } from '../components/TopNav';
+import { useSession } from './SessionContext';
 
 export const Layout = () => {
+  const navigate = useNavigate();
+
+  const { session, profile } = useSession();
+
+  useEffect(() => {
+    if (session && !profile) {
+      navigate('/create-profile');
+    }
+  }, [session, profile]);
+
   return (
     <>
       <TopNav />
