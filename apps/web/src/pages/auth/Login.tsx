e7ef9220679a7d45e16bd6bb4efa4e8d7333dbe6
ASteinheiser <me@iamandrew.io>
2025-05-25 14:06:48 -0700

    implement login page

--- apps/web/src/pages/auth/Login.tsx
+++ apps/web/src/pages/auth/Login.tsx
@@ -1,3 +1,53 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Input, Label, LoadingSpinner } from '@repo/ui';
+import { useSession } from '../../router/SessionContext';
+
 export const Login = () => {
-  return <div>Login</div>;
+  const { login } = useSession();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !password) return;
+
+    setLoading(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold font-title">Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Label>Email</Label>
+        <Input name="email" value={email} onChange={({ target }) => setEmail(target.value)} />
+
+        <Label>Password</Label>
+        <Input
+          name="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+
+        <Button type="submit" disabled={loading}>
+          {loading ? <LoadingSpinner /> : 'Login'}
+        </Button>
+
+        <Button type="button">
+          <Link to="/auth/signup">Signup</Link>
+        </Button>
+      </form>
+    </div>
+  );
 };
