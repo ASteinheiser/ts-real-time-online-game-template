32833039e48fa075c6153cc721cae7fac6bc52aa
ASteinheiser <me@iamandrew.io>
2025-05-31 16:05:38 -0700

    update shared margin styles for nav and footer, slightly adjust footer size

--- apps/web/src/components/Footer/index.tsx
+++ apps/web/src/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
   return (
     <div className="flex flex-col items-center pb-8 pt-12">
-      Made with 💜 by Andrew Steinheiser | Copyright © 2025
+      <span className="font-pixel text-lg">
+        Made with 💜 by Andrew Steinheiser | Copyright © 2025
+      </span>
     </div>
   );
 };
