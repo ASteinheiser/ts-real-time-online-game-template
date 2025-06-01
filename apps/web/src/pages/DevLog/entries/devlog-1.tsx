f7b7cfc458d8f27700ffe521f1013611e348e292
ASteinheiser <me@iamandrew.io>
2025-05-31 18:31:08 -0700

    update devlog demo content

--- apps/web/src/pages/DevLog/entries/devlog-1.tsx
+++ apps/web/src/pages/DevLog/entries/devlog-1.tsx
@@ -7,7 +7,21 @@ export const DevLogEntry1 = () => {
       title="First Steps"
       date="April 24, 2025"
       author="Andrew Steinheiser"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      content={
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/logo.svg" alt="Logo" className="w-1/4 mx-auto" />
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </>
+      }
     />
   );
 };
