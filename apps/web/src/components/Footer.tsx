892770cd4d54e9b91a724ff428452d91e0b15c5e
ASteinheiser <me@iamandrew.io>
2025-06-01 15:21:49 -0700

    lock the footer height, so when it wraps on small screens, stuff stays centered

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,6 +1,9 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-col items-center pb-8 pt-12 px-4">
+    <div
+      className="flex flex-col justify-center items-center pb-8 pt-12 px-4"
+      style={{ height: 'var(--footer-height)' }}
+    >
       <p className="font-label text-lg text-muted-light text-center">
         Made with 💜 by Andrew Steinheiser | Copyright © 2025
       </p>
