df044727532234f57767c1778e13169bb4e6380f
ASteinheiser <me@iamandrew.io>
2025-06-01 11:48:51 -0700

    add border-secondary to global utils, update devLogEntry style

--- apps/web/src/components/DevLogEntry/index.tsx
+++ apps/web/src/components/DevLogEntry/index.tsx
@@ -11,7 +11,7 @@ interface DevLogEntryProps {
 
 export const DevLogEntry = ({ id, title, date, author, content }: DevLogEntryProps) => {
   return (
-    <article className="rounded-xl border-4 border-gray-300">
+    <article className="rounded-3xl border-4 border-secondary">
       <div className="flex flex-col gap-4 p-4">
         <div className="flex flex-row items-center gap-4">
           <h2 className="text-8xl font-isometric isometric-primary">#{id}</h2>
