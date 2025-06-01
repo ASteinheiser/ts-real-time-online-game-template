94680c76e75d9f0ab6e75770fd5515bf588cb459
ASteinheiser <me@iamandrew.io>
2025-06-01 13:03:49 -0700

    move tests to top level folder, flatten components subfolders -- electron

--- apps/electron/src/renderer/src/components/Versions/Versions.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { Versions } from './index';
-
-describe('Versions', () => {
-  it('should exist', () => {
-    expect(Versions).toBeDefined();
-  });
-});
--- apps/electron/test/StartGameForm.test.ts
+++ apps/electron/test/StartGameForm.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { StartGameForm } from './index';
+import { StartGameForm } from '../src/renderer/src/components/StartGameForm';
 
 describe('StartGameForm', () => {
   it('should exist', () => {
