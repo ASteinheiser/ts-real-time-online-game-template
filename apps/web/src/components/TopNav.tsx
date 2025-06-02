98d5b309356b488fa13a666797c9dbee1b29315c
ASteinheiser <me@iamandrew.io>
2025-06-01 17:09:27 -0700

    add sheet menu for mobile navigation

--- apps/web/src/components/TopNav.tsx
+++ apps/web/src/components/TopNav.tsx
@@ -1,34 +1,45 @@
 import { Link, LinkProps, useLocation } from 'react-router-dom';
-import { cn } from '@repo/ui';
+import { cn, Sheet, SheetContent, SheetTrigger } from '@repo/ui';
 import { useSession } from '../router/SessionContext';
 import { Menu } from './icons/Menu';
 
 export const TopNav = () => {
   const { profile } = useSession();
 
+  const renderNavLinks = () => {
+    return (
+      <>
+        <NavLink to="/dev-log">DevLog</NavLink>
+
+        {profile ? (
+          <>
+            <NavLink to="/download">Download</NavLink>
+            <NavLink to="/profile">Profile</NavLink>
+          </>
+        ) : (
+          <NavLink to="/auth/login">Login</NavLink>
+        )}
+      </>
+    );
+  };
+
   return (
-    <div className="fixed z-100 w-full bg-background/50 backdrop-blur-sm">
+    <div className="fixed z-10 w-full bg-background/50 backdrop-blur-sm">
       <div className="max-w-screen-lg mx-auto flex flex-row-reverse sm:flex-row justify-between items-center py-4 px-10">
         <Link to="/">
           <img src="/logo.svg" alt="logo" className="w-12 h-12 hover:animate-pulse" />
         </Link>
 
-        <div className="hidden sm:flex gap-12">
-          <NavLink to="/dev-log">DevLog</NavLink>
+        <div className="hidden sm:flex gap-12">{renderNavLinks()}</div>
 
-          {profile ? (
-            <>
-              <NavLink to="/download">Download</NavLink>
-              <NavLink to="/profile">Profile</NavLink>
-            </>
-          ) : (
-            <NavLink to="/auth/login">Login</NavLink>
-          )}
-        </div>
-
-        <button className="sm:hidden cursor-pointer">
-          <Menu size={36} className="text-muted-light" />
-        </button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="sm:hidden cursor-pointer">
+              <Menu size={36} className="text-muted-light" />
+            </button>
+          </SheetTrigger>
+          <SheetContent side="left">{renderNavLinks()}</SheetContent>
+        </Sheet>
       </div>
     </div>
   );
