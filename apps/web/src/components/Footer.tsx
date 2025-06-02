45ce411caba9d5c38c25c29afba431ba71f39a50
ASteinheiser <me@iamandrew.io>
2025-06-01 17:54:14 -0700

    adjust footer style

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export const Footer = () => {
   return (
     <div
-      className="flex flex-col justify-center items-center pb-8 pt-12 px-4"
+      className="flex flex-col justify-center items-center pb-8 pt-12 px-6"
       style={{ height: 'var(--footer-height)' }}
     >
       <p className="font-label text-lg text-muted-light text-center">
-        Made with 💜 by Andrew Steinheiser | Copyright © 2025
+        Made with 💜 by Andrew Steinheiser - Copyright © 2025
       </p>
     </div>
   );
