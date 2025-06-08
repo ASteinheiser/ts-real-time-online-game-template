c17053a628076548d84f9852afdf2f9b20322d81
ASteinheiser <me@iamandrew.io>
2025-06-08 15:46:08 -0700

    fix env naming

--- apps/game-server/src/auth/client.ts
+++ apps/game-server/src/auth/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
