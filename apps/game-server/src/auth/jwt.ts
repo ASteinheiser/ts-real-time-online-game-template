3511a150ec1540446e0196f4721261d9345af55c
ASteinheiser <me@iamandrew.io>
2025-06-08 01:02:11 -0700

    abstract JWT logic to it's own file

---
+++ apps/game-server/src/auth/jwt.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export interface User {
+  id: string;
+  email: string;
+}
+
+interface DecodedToken {
+  sub: string;
+  email: string;
+}
+
+export const validateJwt = (authHeader?: string): User | null => {
+  if (!authHeader) return null;
+
+  try {
+    const decoded = jwt.verify(authHeader, JWT_SECRET) as DecodedToken;
+
+    if (!decoded.sub || !decoded.email) {
+      throw new Error('Invalid token');
+    }
+
+    return mapDecodedTokenToUser(decoded);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const mapDecodedTokenToUser = (token: DecodedToken): User => {
+  return {
+    id: token.sub,
+    email: token.email,
+  };
+};
--- apps/game-server/src/graphql/context.ts
+++ apps/game-server/src/graphql/context.ts
@@ -1,18 +1,8 @@
 import { ContextFunction } from '@apollo/server';
-import jwt from 'jsonwebtoken';
 import { BooksRepository } from '../repo/Books';
 import { ProfilesRepository } from '../repo/Profiles';
 import type { PrismaClient } from '../prisma-client';
-
-interface User {
-  id: string;
-  email: string;
-}
-
-interface DecodedToken {
-  sub: string;
-  email: string;
-}
+import { validateJwt, type User } from '../auth/jwt';
 
 interface CreateContextArgs {
   prisma: PrismaClient;
@@ -20,29 +10,18 @@ interface CreateContextArgs {
 }
 
 export interface Context {
+  user: User | null;
   dataSources: {
     booksDb: BooksRepository;
     profilesDb: ProfilesRepository;
   };
-  user: User | null;
 }
 
 export const createContext: ContextFunction<[CreateContextArgs], Context> = async ({
   prisma,
   authHeader,
 }) => {
-  let user: User | null = null;
-  if (authHeader) {
-    try {
-      const decoded = jwt.verify(authHeader, process.env.JWT_SECRET) as DecodedToken;
-      if (!decoded.sub || !decoded.email) {
-        throw new Error('Invalid token');
-      }
-      user = mapDecodedTokenToUser(decoded);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const user = validateJwt(authHeader);
 
   return {
     user,
@@ -50,12 +29,5 @@ export const createContext: ContextFunction<[CreateContextArgs], Context> = asyn
       booksDb: new BooksRepository(),
       profilesDb: new ProfilesRepository(prisma),
     },
-  };
-};
-
-const mapDecodedTokenToUser = (token: DecodedToken) => {
-  return {
-    id: token.sub,
-    email: token.email,
   };
 };
