242650473193a974787e20f2eb22272abe740672
ASteinheiser <me@iamandrew.io>
2025-06-14 12:25:01 -0700

    lint

--- packages/client-auth/src/constants.ts
+++ packages/client-auth/src/constants.ts
@@ -1,7 +1,9 @@
 export const SUPABASE_AUTH = {
   HASH: {
-    EMAIL_CHANGE: '#message=Confirmation+link+accepted.+Please+proceed+to+confirm+link+sent+to+the+other+email',
-    LINK_EXPIRED: '#error=access_denied&error_code=otp_expired&error_description=Email+link+is+invalid+or+has+expired',
+    EMAIL_CHANGE:
+      '#message=Confirmation+link+accepted.+Please+proceed+to+confirm+link+sent+to+the+other+email',
+    LINK_EXPIRED:
+      '#error=access_denied&error_code=otp_expired&error_description=Email+link+is+invalid+or+has+expired',
   },
   ERROR: {
     EMAIL_ALREADY_SENT: 'For security purposes, you can only request this after',
