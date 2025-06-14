a52a8d9462b59a01f4c4d5f4bd7f6281fdd628b3
ASteinheiser <me@iamandrew.io>
2025-06-14 12:49:42 -0700

    create AUTH_ROUTES const

--- packages/client-auth/src/constants.ts
+++ packages/client-auth/src/constants.ts
@@ -9,3 +9,11 @@ export const SUPABASE_AUTH = {
     EMAIL_ALREADY_SENT: 'For security purposes, you can only request this after',
   },
 };
+export const AUTH_ROUTES = {
+  LOGIN: '/auth/login',
+  SIGNUP: '/auth/signup',
+  FORGOT_PASSWORD: '/auth/forgot-password',
+  NEW_PASSWORD: '/auth/new-password',
+  CREATE_PROFILE: '/create-profile',
+  PROFILE: '/profile',
+};
