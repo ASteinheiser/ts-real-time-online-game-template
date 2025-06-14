9084f63e105ebff0e04326f7dd31b50521413912
ASteinheiser <me@iamandrew.io>
2025-06-14 12:24:48 -0700

    add apollo client, codegen file and gen types for client-auth

---
+++ packages/client-auth/codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: '../../apps/game-server/src/graphql/schema.graphql',
+  documents: ['src/**/*.ts', 'src/**/*.tsx'],
+  ignoreNoDocuments: true,
+  generates: {
+    './src/graphql/generated-types/': {
+      preset: 'client',
+    },
+  },
+};
+
+export default config;
---
+++ packages/client-auth/src/graphql/generated-types/graphql.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Book = {
+  __typename?: 'Book';
+  author?: Maybe<Scalars['String']['output']>;
+  title?: Maybe<Scalars['String']['output']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createProfile?: Maybe<Profile>;
+  deleteProfile?: Maybe<Scalars['Boolean']['output']>;
+  updateProfile?: Maybe<Profile>;
+};
+
+
+export type MutationCreateProfileArgs = {
+  userName: Scalars['String']['input'];
+};
+
+
+export type MutationUpdateProfileArgs = {
+  userName: Scalars['String']['input'];
+};
+
+export type Profile = {
+  __typename?: 'Profile';
+  userName: Scalars['String']['output'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  books?: Maybe<Array<Maybe<Book>>>;
+  profile?: Maybe<Profile>;
+  userExists?: Maybe<Scalars['Boolean']['output']>;
+};
+
+
+export type QueryUserExistsArgs = {
+  userName: Scalars['String']['input'];
+};
+
+export type Web_GetProfileQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type Web_GetProfileQuery = { __typename?: 'Query', profile?: { __typename?: 'Profile', userName: string } | null };
+
+
+export const Web_GetProfileDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Web_GetProfile"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"profile"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"userName"}}]}}]}}]} as unknown as DocumentNode<Web_GetProfileQuery, Web_GetProfileQueryVariables>;
