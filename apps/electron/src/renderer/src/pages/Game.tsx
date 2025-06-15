ec5f69fefc40ae6ee6f181a5fe103b5a079e8eb8
ASteinheiser <me@iamandrew.io>
2025-06-14 20:13:36 -0700

    grab user session on game page

--- apps/electron/src/renderer/src/pages/Game.tsx
+++ apps/electron/src/renderer/src/pages/Game.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { MAP_WIDTH } from '@repo/core-game';
 import { Button } from '@repo/ui';
+import { useSession } from '@repo/client-auth/provider';
 import { IRefPhaserGame, PhaserGame } from '../game/PhaserGame';
 import { MainMenu } from '../game/scenes/MainMenu';
 import { EventBus } from '../game/EventBus';
@@ -16,13 +17,16 @@ const GET_TOTAL_PLAYERS = gql`
 `;
 
 export const Game = () => {
+  const { session, profile } = useSession();
+  console.log({ session, profile });
+
   const phaserRef = useRef<IRefPhaserGame | null>(null);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data } = useQuery<Desktop_GetTotalPlayersQuery, Desktop_GetTotalPlayersQueryVariables>(
     GET_TOTAL_PLAYERS
   );
-
   console.log({ totalPlayers: data?.totalPlayers ?? 0 });
 
   useEffect(() => {
