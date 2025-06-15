c8668a1407cdab5f032c0d0d3f6400259fbaa166
ASteinheiser <me@iamandrew.io>
2025-06-14 17:22:50 -0700

    abstract useNotifyOnURLHash hook to client-auth

--- packages/client-auth/src/router/index.ts
+++ packages/client-auth/src/router/index.ts
@@ -1,3 +1,4 @@
 export * from './PrivateRoute';
 export * from './constants';
 export * from './useForcedAuthFlow';
+export * from './useNotifyOnURLHash';
---
+++ packages/client-auth/src/router/useNotifyOnURLHash.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { toast } from '@repo/ui';
+import { SUPABASE_AUTH } from '../provider/constants';
+
+export const useNotifyOnURLHash = () => {
+  const location = useLocation();
+  const hashToastRef = useRef('');
+
+  useEffect(() => {
+    // supabase will add this hash to the url when a user clicks
+    // the first of two emails while updating their email address
+    if (
+      location.hash.includes(SUPABASE_AUTH.HASH.EMAIL_CHANGE) &&
+      hashToastRef.current !== location.hash
+    ) {
+      hashToastRef.current = location.hash;
+      toast.success('Please click the confirmation link sent to the other email', {
+        duration: 10000,
+      });
+    }
+    // supabase will add this hash when a user clicks an expired email link
+    if (
+      location.hash.includes(SUPABASE_AUTH.HASH.LINK_EXPIRED) &&
+      hashToastRef.current !== location.hash
+    ) {
+      hashToastRef.current = location.hash;
+      toast.error('Email link is invalid or has expired', { duration: 10000 });
+    }
+  }, [location.hash]);
+};
