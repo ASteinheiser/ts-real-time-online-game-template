0a785b935021b5c9abcdf939e00894ca53243e66
ASteinheiser <me@iamandrew.io>
2025-06-14 17:01:40 -0700

    abstract hook for useScrollToTopOnPageChange to ui package

---
+++ packages/ui/src/hooks/useScrollToTopOnPageChange.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const useScrollToTopOnPageChange = () => {
+  const location = useLocation();
+
+  // ensure the user starts at the top of the page when navigating
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+};
