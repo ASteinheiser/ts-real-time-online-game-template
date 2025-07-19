bf91d01a2dc5fbc4036ad66e14d6d1ea3b1371df
ASteinheiser <me@iamandrew.io>
2025-07-19 13:20:25 -0700

    show optionsModal on auth pages for desktop app

--- apps/desktop/src/renderer/src/router/Layout.tsx
+++ apps/desktop/src/renderer/src/router/Layout.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useForcedAuthFlow, useNotifyOnURLHash, useIsAuthRoute } from '@repo/client-auth/router';
+import { Settings } from '@repo/ui/icons';
 import { MAP_SIZE } from '@repo/core-game';
 import { APP_ROUTES } from './constants';
 import logo from '../assets/logo.png';
+import { OptionsModal } from '../modals/OptionsModal';
 
 export const Layout = () => {
   const isAuthRoute = useIsAuthRoute();
+  const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
 
   useForcedAuthFlow({
     alreadyLoggedInRedirectPath: APP_ROUTES.GAME,
@@ -14,7 +18,17 @@ export const Layout = () => {
 
   return (
     <div className="fullscreen-center" style={{ width: MAP_SIZE.width, height: MAP_SIZE.height }}>
-      {isAuthRoute && <img src={logo} className="h-30 w-auto mb-10 mt-[-40px]" />}
+      {isAuthRoute && (
+        <>
+          <div className="fixed top-6 right-6 z-2 cursor-pointer" onClick={() => setIsOptionsModalOpen(true)}>
+            <Settings className="text-muted" size={36} />
+          </div>
+
+          <OptionsModal isOpen={isOptionsModalOpen} onOpenChange={setIsOptionsModalOpen} />
+
+          <img src={logo} className="h-30 w-auto mb-10 mt-[-40px]" />
+        </>
+      )}
 
       <Outlet />
     </div>
