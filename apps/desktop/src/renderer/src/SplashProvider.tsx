19d087e8924f4b57eda2a260c0b56df4555d7402
ASteinheiser <me@iamandrew.io>
2025-07-19 02:57:16 -0700

    create basic splash screen provider (for showing game dev/publisher logo)

---
+++ apps/desktop/src/renderer/src/SplashProvider.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import logo from './assets/logo.png';
+
+const SPLASH_DURATION = 3 * 1000; // 3 seconds
+
+interface SplashProviderProps {
+  children: React.ReactNode;
+}
+/**
+ * This will play only once when the app is first opened.
+ * Good place to put developer and/or publisher logos.
+ */
+export const SplashProvider = ({ children }: SplashProviderProps) => {
+  const [isSplashVisible, setIsSplashVisible] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsSplashVisible(false), SPLASH_DURATION);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <>
+      {isSplashVisible && (
+        <div className="fullscreen-center">
+          <img src={logo} className="h-40 w-auto mb-15" />
+          <h1 className="text-5xl font-pixel text-primary">iamandrew.io games</h1>
+        </div>
+      )}
+      {children}
+    </>
+  );
+};
