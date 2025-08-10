76c6940c7d8e07b44d90dd4f52f5085adfa4d6aa
ASteinheiser <me@iamandrew.io>
2025-08-10 16:48:22 -0700

    style the ping display

--- apps/desktop/src/renderer/src/game/objects/PingDisplay.ts
+++ apps/desktop/src/renderer/src/game/objects/PingDisplay.ts
@@ -3,19 +3,33 @@ import type { Scene } from 'phaser';
 import { CustomText } from './CustomText';
 import { WS_EVENT } from '@repo/core-game';
 
+const MARGIN = 10;
+const PADDING = 6;
+const CORNER_RADIUS = 8;
+
 export class PingDisplay {
   scene: Scene;
   room?: Room;
   pingStartTime = 0;
   currentPingMs = 0;
   timerEvent?: Phaser.Time.TimerEvent;
+  background: Phaser.GameObjects.Graphics;
   pingText: CustomText;
 
   constructor(scene: Scene) {
     this.scene = scene;
-    this.pingText = new CustomText(this.scene, 10, 10, 'Ping: --', { fontSize: '16px', color: '#00ff00' })
-      .setOrigin(0, 0)
+
+    const x = this.scene.cameras.main.width - MARGIN;
+    const y = MARGIN;
+    this.pingText = new CustomText(this.scene, x, y, '--', {
+      fontSize: '16px',
+      color: '#00ff00',
+    })
+      .setOrigin(1, 0)
       .setScrollFactor(0);
+
+    this.background = this.scene.add.graphics().setScrollFactor(0).setDepth(99);
+    this.updateBackgroundSize();
   }
 
   start(room: Room) {
@@ -43,9 +57,10 @@ export class PingDisplay {
 
   updateDisplay() {
     const color = this.getPingColor(this.currentPingMs);
-    const pingText = `Ping: ${this.currentPingMs}ms`;
+    const pingText = `${this.currentPingMs}ms`;
 
     this.pingText.setColor(color).setText(pingText);
+    this.updateBackgroundSize();
   }
 
   getPingColor(pingMs: number): string {
@@ -55,9 +70,20 @@ export class PingDisplay {
     return '#ff0000';
   }
 
+  updateBackgroundSize() {
+    const bgWidth = this.pingText.displayWidth + PADDING * 2;
+    const bgHeight = this.pingText.displayHeight + PADDING * 2;
+    const bgX = this.pingText.x - bgWidth + PADDING;
+    const bgY = this.pingText.y - PADDING;
+
+    this.background.clear();
+    this.background.fillStyle(0x000000, 0.5).fillRoundedRect(bgX, bgY, bgWidth, bgHeight, CORNER_RADIUS);
+  }
+
   destroy() {
     this.timerEvent?.remove();
     delete this.timerEvent;
     this.pingText.destroy();
+    this.background.destroy();
   }
 }
