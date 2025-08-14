eee602d3845cccad8b4f7271c4c241ca4b385cd5
ASteinheiser <me@iamandrew.io>
2025-08-13 21:59:38 -0700

    update supabase client settings and export client

--- packages/client-auth/src/provider/supabase-client.ts
+++ packages/client-auth/src/provider/supabase-client.ts
@@ -5,4 +5,12 @@ const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if (!SUPABASE_URL) throw new Error('VITE_SUPABASE_URL is not set');
 if (!SUPABASE_ANON_KEY) throw new Error('VITE_SUPABASE_ANON_KEY is not set');
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    flowType: 'pkce',
+    // We disable `detectSessionInUrl` because our custom redirect page will handle the exchange (and deep link if needed)
+    detectSessionInUrl: false,
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
