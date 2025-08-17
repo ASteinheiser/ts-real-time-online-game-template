ecf5fb9801beed2e82c447a7e57e6d0dd07544fd
ASteinheiser <me@iamandrew.io>
2025-08-16 17:47:57 -0700

    simpler hash toast logic (no refs needed)

--- packages/client-auth/src/router/useNotifyOnURLHash.ts
+++ packages/client-auth/src/router/useNotifyOnURLHash.ts
@@ -1,26 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { toast } from '@repo/ui';
 import { SUPABASE_AUTH } from '../provider/constants';
 
+const TOAST_DURATION = 10000;
+
 /** Handles showing a toast to the user when they click on a supabase email link */
 export const useNotifyOnURLHash = () => {
   const { hash } = useLocation();
-  const hashToastRef = useRef('');
 
   useEffect(() => {
     // supabase will add this hash to the url when a user clicks
     // the first of two emails while updating their email address
-    if (hash.includes(SUPABASE_AUTH.HASH.EMAIL_CHANGE) && hashToastRef.current !== hash) {
-      hashToastRef.current = hash;
+    if (hash.includes(SUPABASE_AUTH.HASH.EMAIL_CHANGE)) {
       toast.success('Please click the confirmation link sent to the other email', {
-        duration: 10000,
+        duration: TOAST_DURATION,
       });
     }
     // supabase will add this hash when a user clicks an expired email link
-    if (hash.includes(SUPABASE_AUTH.HASH.LINK_EXPIRED) && hashToastRef.current !== hash) {
-      hashToastRef.current = hash;
-      toast.error('Email link is invalid or has expired', { duration: 10000 });
+    if (hash.includes(SUPABASE_AUTH.HASH.LINK_EXPIRED)) {
+      toast.error('Email link is invalid or has expired', {
+        duration: TOAST_DURATION,
+      });
     }
   }, [hash]);
 };
