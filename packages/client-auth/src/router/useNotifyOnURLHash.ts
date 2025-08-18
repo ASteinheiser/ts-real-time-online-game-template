36d23ae15ef5402ae1b80affa0ced4bbda8283f8
ASteinheiser <me@iamandrew.io>
2025-08-17 21:46:37 -0700

    update hash notification hook to consume hashes to prevent duplicate toasts

--- packages/client-auth/src/router/useNotifyOnURLHash.ts
+++ packages/client-auth/src/router/useNotifyOnURLHash.ts
@@ -1,5 +1,5 @@
-import { useEffect, useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from '@repo/ui';
 import { SUPABASE_AUTH } from '../provider/constants';
 import { AUTH_SEARCH_PARAMS } from './constants';
@@ -8,20 +8,19 @@ const TOAST_DURATION = 10000;
 
 /** Handles showing a toast to the user when they click on a supabase email link */
 export const useNotifyOnURLHash = () => {
-  const { search, hash } = useLocation();
-
-  const isDeepLink = useMemo(() => {
-    const searchParams = new URLSearchParams(search);
-    return searchParams.get(AUTH_SEARCH_PARAMS.DEEP_LINK);
-  }, [search]);
+  const navigate = useNavigate();
+  const { pathname, search, hash } = useLocation();
 
   useEffect(() => {
-    if (isDeepLink) return;
+    const searchParams = new URLSearchParams(search);
+    // don't show toasts if we're opening a deep link (/auth/redirect)
+    if (searchParams.get(AUTH_SEARCH_PARAMS.DEEP_LINK)) return;
+
+    let didNotify = false;
     // handle custom hash for when email change is successful
     if (hash.includes(SUPABASE_AUTH.HASH.EMAIL_CHANGE_SUCCESS)) {
-      toast.success('Email changed successfully', {
-        duration: TOAST_DURATION,
-      });
+      toast.success('Email changed successfully', { duration: TOAST_DURATION });
+      didNotify = true;
     }
     // supabase will add this hash to the url when a user clicks the first of two emails while updating their email address
     // ONLY IF `Secure email change` is set to `true`; see `README.md` for more info
@@ -29,12 +28,17 @@ export const useNotifyOnURLHash = () => {
       toast.success('Please click the confirmation link sent to the other email', {
         duration: TOAST_DURATION,
       });
+      didNotify = true;
     }
     // supabase will add this hash when a user clicks an expired email link
     if (hash.includes(SUPABASE_AUTH.HASH.LINK_EXPIRED)) {
-      toast.error('Email link is invalid or has expired', {
-        duration: TOAST_DURATION,
-      });
+      toast.error('Email link is invalid or has expired', { duration: TOAST_DURATION });
+      didNotify = true;
     }
-  }, [hash, isDeepLink]);
+
+    // consume the hash by replacing the current URL without it to prevent duplicate toasts
+    if (didNotify && hash) {
+      navigate(`${pathname}${search}`, { replace: true });
+    }
+  }, [hash, pathname, search, navigate]);
 };
