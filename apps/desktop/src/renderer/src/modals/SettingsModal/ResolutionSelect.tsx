4576706278637040be83198e9e7c346df6d30f93
ASteinheiser <me@iamandrew.io>
2025-08-24 13:50:08 -0700

    create basic resolution select component

---
+++ apps/desktop/src/renderer/src/modals/SettingsModal/ResolutionSelect.tsx
@@ -0,0 +1,37 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@repo/ui';
+
+export interface ResolutionOption {
+  width: number;
+  height: number;
+}
+
+interface ResolutionSelectProps {
+  availableResolutions: Array<ResolutionOption>;
+  currentResolution: string;
+  onResolutionChange: (resolution: string) => void;
+}
+
+export const ResolutionSelect = ({
+  availableResolutions,
+  currentResolution,
+  onResolutionChange,
+}: ResolutionSelectProps) => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[130px]">
+        <SelectValue placeholder="--" />
+      </SelectTrigger>
+
+      <SelectContent defaultValue={currentResolution}>
+        {availableResolutions.map(({ width, height }) => {
+          const resolution = `${width}x${height}`;
+          return (
+            <SelectItem key={resolution} value={resolution} onClick={() => onResolutionChange(resolution)}>
+              {resolution}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
+};
