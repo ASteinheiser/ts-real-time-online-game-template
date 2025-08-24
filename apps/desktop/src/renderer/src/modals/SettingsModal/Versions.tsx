803c57a469dce29f31d4bb2d5a7cb0b0add4d77d
ASteinheiser <me@iamandrew.io>
2025-08-24 13:30:41 -0700

    style versions component

--- apps/desktop/src/renderer/src/modals/SettingsModal/Versions.tsx
+++ apps/desktop/src/renderer/src/modals/SettingsModal/Versions.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
+import { Electron, Chromium, Node } from '@repo/ui/icons';
 
 export const Versions = () => {
   const [versions] = useState(window.electron.process.versions);
 
   return (
     <ul className="flex flex-row gap-4">
-      <li className="text-md font-title">Electron v{versions.electron}</li>
-      <li className="text-md font-title">Chromium v{versions.chrome}</li>
-      <li className="text-md font-title">Node v{versions.node}</li>
+      <li className="text-sm font-label flex flex-row items-center">
+        <Electron size={20} className="mr-2" />v{versions.electron}
+      </li>
+      <li className="text-sm font-label flex flex-row items-center">
+        <Chromium size={20} className="mr-2" /> v{versions.chrome}
+      </li>
+      <li className="text-sm font-label flex flex-row items-center">
+        <Node size={20} className="mr-2" /> v{versions.node}
+      </li>
     </ul>
   );
 };
